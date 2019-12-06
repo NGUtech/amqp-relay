34fc2307d762e13633f29e1bdb4298891170bf42
Hasham Ahmad <mr.hash@gmail.com>
2019-12-06 18:39:21 +0000

    🕹

---
+++ README.md
@@ -0,0 +1,27 @@
+# AMQP notification relay plugin for c-lightning
+This plugin may be useful for your consumer application to process notification from an array
+of lightning nodes or ensuring that no notifcations are missed in case of an application failure.
+
+This has been tested with RabbitMQ.
+
+## Dependencies
+ - `clightningjs`[https://github.com/darosior/clightningjs]
+ - `amqplib`[https://github.com/squaremo/amqp.node]
+ - `zermomq`[https://github.com/zeromq/zeromq.js/]
+
+## Installation
+ - Clone plugin to your c-lightning plugin directory.
+ - Ensure `amqp-relay.js` is executable.
+ - Run `npm install` to install required dependencies.
+
+## Configuration
+This plugin connects to an AMQP exchange specified with startup options as follows:
+
+ - `amqp-host`: the host address in address:port format
+ - `amqp-auth`: the optional service credentials in user:pass format
+ - `amqp-exchange`: the name of the exchange you wish to send through
+ - `amqp-notification`: a comma seperated list of notification to relay
+
+It will attempt reconnection every 10 seconds on startup if a host, exchange & notification list are provided.
+
+There are no exposed RPC methods and no c-lightning hooks are used.
---
+++ amqp-relay.js
@@ -0,0 +1,85 @@
+#!/usr/bin/env node
+const amqplib = require('amqplib/callback_api');
+const Plugin = require('clightningjs');
+
+const relayPlugin = new Plugin();
+let amqp, auth, host, exchange, connectionInterval;
+let enabledNotifications = {};
+
+relayPlugin.onInit = function(params) {
+  if (params.options['amqp-host'] !== 'off') {
+    host = params.options['amqp-host'];
+  }
+
+  if (params.options['amqp-auth'] !== 'off') {
+    auth = params.options['amqp-auth'];
+  }
+
+  if (params.options['amqp-exchange'] !== 'off') {
+    exchange = params.options['amqp-exchange'];
+  }
+
+  if (params.options['amqp-notifications'] !== 'off') {
+    params.options['amqp-notifications'].split(',').forEach(
+      event => {
+        enabledNotifications[event] = true;
+      }
+    );
+  }
+
+  if (host && exchange && Object.keys(enabledNotifications).length > 0) {
+    connectQueue();
+    connectionInterval = setInterval(connectQueue, 10000);
+  }
+
+  return true;
+};
+
+function connectQueue() {
+  relayPlugin.log('Connecting to AMQP service at ' + host);
+  const endpoint = auth ? auth+'@'+host : host;
+  amqplib.connect('amqp://'+endpoint, function(err, conn) {
+    if (err != null) {
+      relayPlugin.log('AMQP connection error: '+err.code);
+      return;
+    }
+    conn.createChannel(function(err, ch) {
+      if (err != null) {
+        relayPlugin.log('AMQP channel error: '+err.code);
+        return;
+      }
+      amqp = ch;
+      clearInterval(connectionInterval);
+      ch.assertExchange(exchange, 'topic', {durable: true, autoDelete: false});
+      relayPlugin.log('AMQP channel connection established.');
+    })
+  });
+}
+
+function publish(event, message) {
+  if (amqp && enabledNotifications[event] === true) {
+    amqp.publish(exchange, 'clightning.'+event, Buffer.from(JSON.stringify(message)));
+  }
+}
+
+relayPlugin.addOption('amqp-auth', 'off', 'AMQP service user:pass credentials', 'string');
+relayPlugin.addOption('amqp-host', 'off', 'AMQP service host:port address', 'string');
+relayPlugin.addOption('amqp-exchange', 'off', 'AMQP service target exchange', 'string');
+relayPlugin.addOption('amqp-notifications', 'off', 'AMQP notification relay list', 'string');
+
+relayPlugin.subscribe('channel_opened');
+relayPlugin.subscribe('connect');
+relayPlugin.subscribe('disconnect');
+relayPlugin.subscribe('invoice_payment');
+relayPlugin.subscribe('forward_event');
+relayPlugin.subscribe('sendpay_success');
+relayPlugin.subscribe('sendpay_failure');
+relayPlugin.notifications.channel_opened.on('channel_opened', message => {publish('channel_opened', message)});
+relayPlugin.notifications.connect.on('connect', message => {publish('connect', message)});
+relayPlugin.notifications.disconnect.on('disconnect', message => {publish('disconnect', message)});
+relayPlugin.notifications.invoice_payment.on('invoice_payment', message => {publish('invoice_payment', message)});
+relayPlugin.notifications.forward_event.on('forward_event', message => {publish('forward_event', message)});
+relayPlugin.notifications.sendpay_success.on('sendpay_success', message => {publish('sendpay_success', message)});
+relayPlugin.notifications.sendpay_failure.on('sendpay_failure', message => {publish('sendpay_failure', message)});
+
+relayPlugin.start();
